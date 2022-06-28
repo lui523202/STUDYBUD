caa043d03c166d712a7b45fab5ed3f59cdbaa4c5
Louie <jannlouiealmirante@gmail.com>
2022-06-28 16:52:10 +0800

    Create comment section in rooms

--- base/views.py
+++ base/views.py
@@ -86,14 +86,9 @@ def home(request):
 
 
 def room(request, pk):
-    # room = None
-
-    # for i in rooms:
-    #     if i['id'] == int(pk):
-    #         room = i
     room = Room.objects.get(id=pk)
-    context = {'room': room}
-
+    room_messages = room.message_set.all().order_by('-created')
+    context = {'room': room, 'room_messages': room_messages}
     return render(request, 'base/room.html', context)
 
 
