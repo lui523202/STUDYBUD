11cde05806d717a6561c3a5b98b4c82346e32df4
Louie <jannlouiealmirante@gmail.com>
2022-07-01 13:15:23 +0800

    Add update-user function to profile and clean 'cancel' links

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from django.http import HttpResponse
 
 # Create your views here.
@@ -184,6 +184,7 @@ def deleteRoom(request, pk):
 
     return render(request, 'base/delete.html', {'obj': room})
 
+
 @login_required(login_url='login')
 def deleteMessage(request, pk):
     message = Message.objects.get(id=pk)
@@ -196,3 +197,19 @@ def deleteMessage(request, pk):
         return redirect('home')
 
     return render(request, 'base/delete.html', {'obj': message})
+
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+
+    context = {'form':form}
+    return render(request, 'base/update-user.html', context)
